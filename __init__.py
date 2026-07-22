9b2258136f72626b9074c844d1ef397f0a863a62
josueprlt <148058543+josueprlt@users.noreply.github.com>
2026-07-22 06:35:49 +0000

    Add linter ruff + Add new test

--- __init__.py
+++ __init__.py
@@ -1,17 +1,14 @@
-from flask import Flask
-from flask import render_template
-from flask import json
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
-@app.route('/')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
 def hello_world():
     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
 
-@app.route('/exercices/')
+@app.route("/exercices/")
 def exercices():
-    return render_template('exercices.html')
-                                                                                                               
+    return render_template("exercices.html")
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host="0.0.0.0", debug=True)
