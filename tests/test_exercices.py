b19ff96741dae4d7e8a5f7e990d84e0cb3982ade
josueprlt <148058543+josueprlt@users.noreply.github.com>
2026-07-22 06:41:07 +0000

    ex: tests KO / lint OK

--- tests/test_exercices.py
+++ tests/test_exercices.py
@@ -2,5 +2,5 @@ def test_exercices_page_status_and_content(client):
     response = client.get("/exercices/")
 
     assert response.status_code == 200
-    assert b"Bienvenue sur votre page Web" in response.data
+    assert b"Bienvenue sur vote pge Web" in response.data
     assert b"Ceci est le projet de" in response.data
